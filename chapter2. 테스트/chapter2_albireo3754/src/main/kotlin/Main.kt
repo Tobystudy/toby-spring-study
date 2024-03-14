fabf082ab1b3d65493fdda62de2cac7da182d3a3
alibreo3754 <starpro123@naver.com>
2024-03-14 22:34:19 +0900

    테스트 자동화 커밋 1

--- chapter2. 테스트/chapter2_albireo3754/src/main/kotlin/Main.kt
+++ chapter2. 테스트/chapter2_albireo3754/src/main/kotlin/Main.kt
@@ -11,21 +11,17 @@ fun main(args: Array<String>) {
     val application = runApplication<Main>(*args)
 
     val userDao = application.getBean<UserDao>()
-    val userDao2 = application.getBean<UserDao>()
-
-    val factory = application.getBean<DaoFactory>()
-
-    println(DaoFactory().userDao(connectionMaker = factory.connectionMaker()))
-    println(DaoFactory().userDao(connectionMaker = factory.connectionMaker()))
-    println(DaoFactory().userDao(connectionMaker = factory.connectionMaker()) == DaoFactory().userDao(connectionMaker = factory.connectionMaker()))
-    println(userDao == userDao2)
 
     val user = User("whiteship", "백기선", "married")
     userDao.add(user)
 
-    println("${user.id} 등록 성공!!")
-
     val user2 = userDao.get(user.id)
-    println(user2.password)
-    println("${user2.id} 조회 성공!!")
+
+    if (user.name != user2.name) {
+        println("테스트 실패 ${user.name} != ${user2.name} !!")
+    } else if (user.password != user2.password) {
+        println("테스트 실패 ${user.password} != ${user2.password} !!")
+    } else {
+        println("${user.id} 조회 성공!!")
+    }
 }
