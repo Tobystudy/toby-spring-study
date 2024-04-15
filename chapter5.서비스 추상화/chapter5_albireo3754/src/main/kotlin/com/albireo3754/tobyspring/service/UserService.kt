eb0c50a49e1d52e67c2adcb88c397061e1f3328d
alibreo3754 <starpro123@naver.com>
2024-04-15 21:18:31 +0900

    커밋 6 - JavaMail을 작성하였다.

--- chapter5.서비스 추상화/chapter5_albireo3754/src/main/kotlin/com/albireo3754/tobyspring/service/UserService.kt
+++ chapter5.서비스 추상화/chapter5_albireo3754/src/main/kotlin/com/albireo3754/tobyspring/service/UserService.kt
@@ -3,9 +3,16 @@ package com.albireo3754.tobyspring.service
 import com.albireo3754.domain.Level
 import com.albireo3754.domain.User
 import com.albireo3754.tobyspring.dao.UserDao
+import jakarta.mail.Address
+import jakarta.mail.Session
+import jakarta.mail.Transport
+import jakarta.mail.internet.InternetAddress
+import jakarta.mail.internet.MimeMessage
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.mail.javamail.MimeMailMessage
 import org.springframework.stereotype.Component
 import org.springframework.transaction.support.DefaultTransactionDefinition
+import java.util.Properties
 
 @Component
 class UserService(
@@ -41,5 +48,22 @@ class UserService(
     fun upgradeLevel(user: User) {
         user.upgradeLevel()
         userDao.update(user)
+        sendUpgradeEMail(user)
+    }
+
+    private fun sendUpgradeEMail(user: User) {
+        val props = Properties()
+        props["mail.smtp.host"] = "mail.albireo3754.com"
+        try {
+            val mail = MimeMessage(Session.getInstance(props))
+            mail.setFrom("starpro123@naver.com")
+            mail.addRecipient(MimeMessage.RecipientType.TO, InternetAddress("starpro123@naver.com"))
+            mail.subject = "Upgrade 안내"
+            mail.setText("${user.id}님의 등급이 ${user.level.name}로 업그레이드 되었습니다.")
+            Transport.send(mail)
+        } catch (e: Exception) {
+            println(e)
+            throw RuntimeException(e)
+        }
     }
 }
